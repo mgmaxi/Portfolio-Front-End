283406987b6f2294276cfd9e0b702e7f4d8cc5bc
mgmaxi <maximiliano.ghirardi@gmail.com>
2022-01-07 16:59:58 -0300

    Add router

---
+++ src/app/app-routing.module.ts/app-routing.module.ts.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from '../components/home/home.component';
+import { MainComponent } from '../components/main/main.component';
+import { LoginComponent } from '../components/login/login.component';
+import { SignupComponent } from '../components/signup/signup.component';
+import { NotFoundComponent } from '../components/not-found/not-found.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'portfolio', component: MainComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent },
+];
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
